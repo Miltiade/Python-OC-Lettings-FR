7b3ea7ea7287f35bbad2187bc6bdf4d6235706b3
Miltiade <vero@tutamail.com>
2026-09-11 10:12:24 +0300

    refactor: per-app URLs with namespaces, views moved and renamed to index (W2-T6)

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Profile
+
+def index(request):
+    """List all profiles."""
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+def profile(request, username):
+    """Display a single user's profile."""
+    profile = Profile.objects.get(user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
